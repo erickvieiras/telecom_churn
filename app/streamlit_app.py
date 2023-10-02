47ddf62b970a2ac52028d73447bae78d213f4dbc
ErickVieira <erickvieiras.ds@gmail.com>
2023-10-02 08:35:36 -0300

    testing fix error groupby

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -72,7 +72,7 @@ with tab1:
     st.markdown(df['churn'].dtypes)
     df_teste = df.copy()
     #teste = df[(df['churn'] == 'Yes')]
-    #teste = teste[['monthly_charge', 'churn', 'internet_type']].groupby(['internet_type']).mean().reset_index()
+    df_teste = df_teste[['monthly_charge', 'churn', 'internet_type']].groupby(['internet_type']).sum().reset_index()
     st.dataframe(df_teste)
         
     with st.expander('Dataset Information'):
